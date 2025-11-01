c3e70cfaa2d8a9f2ca46721dda6e5d4213be4c59
Leonserka <leonserka44@gmail.com>
2025-11-01 19:24:02 +0100

    Added Thymeleaf support, homepage controller, and updated .gitignore

--- src/main/java/com/bus/bus_tracker/repository/FavoriteRepository.java
+++ src/main/java/com/bus/bus_tracker/repository/FavoriteRepository.java
@@ -1,6 +1,6 @@
 package com.bus.bus_tracker.repository;
 
-import com.bus.bus_tracker.model.Favorite;
+import com.bus.bus_tracker.entity.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/bus/bus_tracker/repository/SalesPointRepository.java
+++ src/main/java/com/bus/bus_tracker/repository/SalesPointRepository.java
@@ -1,6 +1,6 @@
 package com.bus.bus_tracker.repository;
 
-import com.bus.bus_tracker.model.SalesPoint;
+import com.bus.bus_tracker.entity.SalesPoint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
