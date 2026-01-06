cdc3386c1ed7d42c081ddb32f457551eec68b84a
NikolaKaramarko <185039725+nk128271@users.noreply.github.com>
2026-01-06 15:35:37 +0100

    Added Sales point feature + map of its locations

---
+++ src/main/resources/db/migration/V25__sales_points_opening_hoursfix.sql
@@ -0,0 +1,11 @@
+-- Kastel Stari
+UPDATE sales_points
+SET opening_hours = 'od Ponedjeljka do Petka 07:00 - 14:00
+Prva Subota u mjesecu 07:00 - 12:30'
+WHERE name = 'Kaštel Stari';
+
+-- Kastel Sucurac
+UPDATE sales_points
+SET opening_hours = 'od Ponedjeljka do Petka 07:00 - 13:30
+Prva Subota u mjesecu 07:00 - 12:30'
+WHERE name = 'Kaštel Sućurac';
---
+++ src/main/resources/db/migration/V26__add_opening_hours_to_sales_points.sql
@@ -0,0 +1,5 @@
+UPDATE sales_points
+SET
+    gps_lat = 43.55409337838537,
+    gps_lng = 16.347124454998315
+WHERE name = 'Kaštel Stari';
